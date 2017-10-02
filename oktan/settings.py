2d1d0e92fcc35fbd9f36f1d2a68722a861d2899a
Ali Akbar <aliakbar@Alis-MacBook-Pro.local>
2017-10-02 15:18:30 +0700

    Fix missing import

--- oktan/settings.py
+++ oktan/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,8 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False;
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')
     )
 }
 
