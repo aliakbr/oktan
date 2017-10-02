2ccfc4639a515c815653ad42db44cf5f44591e4a
Ali Akbar <aliakbar@Alis-MacBook-Pro.local>
2017-10-02 15:16:28 +0700

    Turn off debug mode

--- oktan/settings.py
+++ oktan/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # DEBUG = True
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG', default=False, cast=bool)
+DEBUG = False;
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
